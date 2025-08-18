4baa5af611999e8744b7d7b2f8b09edb7b8ecdd8
salimettayeb <selim.ettayeb@gmail.com>
2025-08-18 23:48:39 +0100

    create project backend

---
+++ back-end-maximo/src/test/java/com/backendmaximo/BackEndMaximoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendmaximo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndMaximoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
